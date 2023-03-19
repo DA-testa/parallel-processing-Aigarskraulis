22b583c586563aecf01c102ad9009a4b972f28a4
Aigarskraulis <62945107+Aigarskraulis@users.noreply.github.com>
2023-03-19 17:54:22 +0000

    testo

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#phyton3
+#221RDC045 Aigars Kraulis 18.grupa
+
 def process_in_parallel(n, m, data):
     threads = list(range(n))
     times = [0] * n
@@ -18,9 +21,18 @@ def process_in_parallel(n, m, data):
     return output
 
 def main():
+      # TODO: create input from keyboard
+    # input consists of two lines
+    # first line - n and m
+    # n - thread count 
+    # m - job count
     n, m = map(int, input().split())
+    # second line - data 
+    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
     data = list(map(int, input().split()))
+      # TODO: create the function
     result = process_in_parallel(n, m, data)
+      # TODO: print out the results, each pair in it's own line
     for i in range(0, len(result), 2):
         print(result[i], result[i+1])
 
