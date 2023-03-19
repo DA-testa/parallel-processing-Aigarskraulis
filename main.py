354d0268ed50b32f9abdf90675b75be0ba928647
Aigarskraulis <62945107+Aigarskraulis@users.noreply.github.com>
2023-03-19 17:50:08 +0000

    testt48

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
 #python3
 # 221RDC045 Aigars Kraulis 18.grupa
 
-def parallel_processing(n, m, data):
-    threadList = []
-    timeList = []
+def process_in_parallel(n, m, data):
+    threads = list(range(n))
+    times = [0] * n
     output = []
-    for i in range(n):
-        threadList.append(i)
-        timeList.append(0)
-
     counter = 0
     time = 0
     for i in range(m):
-        output.append(threadList[counter])
-        output.append(timeList[counter])
+        output.append(threads[counter])
+        output.append(times[counter])
         while True:
-            timeList[counter] = timeList[counter] + 1
-            data[i] = data[i] - 1
+            times[counter] += 1
+            data[i] -= 1
             if data[i] == 0:
                 break
-        
-        counter += 1
-        if counter == len(threadList):
-            counter = 0
+        counter = (counter + 1) % n
+        if counter == 0:
             time += 1
-
     return output
 
 def main():
@@ -33,10 +26,7 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    firstInput = input()
-    splitInput = firstInput.split()
-    n = int(splitInput[0])
-    m = int(splitInput[1])
+     n, m = map(int, input().split())
     
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
@@ -54,53 +44,3 @@ if __name__ == "__main__":
     main()
 
 
-def parallel_processing(n, m, data):
-    output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
-    threadList = []
-    timeList = []
-    for i in range(n):
-        threadList.append(i)
-        timeList.append(0)
-
-    counter = 0
-    time = 0
-    for i in range(m):
-        output.append(threadList[counter])
-        output.append(timeList[counter])
-        while True:
-            timeList[counter] = timeList[counter] + 1
-            data[i] = data[i] - 1
-            if data[i] == 0:
-                break
-        
-        counter += 1
-        if counter == len(threadList):
-            counter = 0
-            time += 1
-
-    return output
-
-def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
-
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
-
-    # TODO: create the function
-    result = parallel_processing(n, m, data)
-    
-    # TODO: print out the results, each pair in it's own line
-
-
-
-if __name__ == "__main__":
-    main()
